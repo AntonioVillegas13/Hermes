a079dbd09e74800988b5782cbe2487b8785e7cd8
AntonioVillegas13 <antonio.villegas@epn.edu.ec>
2022-12-19 18:12:22 -0500

    tododddd

--- app/screens/ClienteScreen/PedidosScreen.js
+++ app/screens/ClienteScreen/PedidosScreen.js
@@ -17,7 +17,10 @@ export const ListaPedidosFinal = ({ navigation }) => {
         console.log("recupernado datos ")
         consultar(setPedidos);
         //console.log("OED", pedidos);
+        console.log("Uid",global.userIdLogin)
         pedidos2=pedidos.filter(item =>item.codigo ==global.userIdLogin)
+        console.log("PEDIDOS2",pedidos2)
+        console.log("PEDIDOS",pedidos)
     // console.log("LISTA PEDIDO;",pedidos[0].codigo)
 
 
@@ -33,64 +36,11 @@ export const ListaPedidosFinal = ({ navigation }) => {
 
 
 
-    // let ItemProduct = ({ prod, indice }) => {
-
-    //     return (
-
-    //         <ScrollView style={styles.impar} >
-    //             <TouchableHighlight onPress={() => {
-    //             }}>
-    //                 <View style={{ flexDirection: 'row', alignContent: 'flex-start', flex: 1 }}>
-
-
-
-    //                     <View style={{ flexDirection: 'column', alignContent: 'flex-start', flex: 1, alignItems: 'center', alignContent: 'center', marginTop: 20 }}>
-    //                         <Text>{prod.codigo}</Text>
-    //                     </View>
-
-    //                     <View style={{ flexDirection: 'column', alignContent: 'flex-start', flex: 4 }}>
-    //                         <Text style={{ fontSize: 20, paddingLeft: 4, color: '#FAC028', fontWeight: 'bold' }}>{prod.nombre}</Text>
-    //                         <Text style={{ fontSize: 15, fontStyle: 'italic', textAlign: 'left', color: "#eeeeee" }}> ({prod.categoria})</Text>
-    //                     </View>
-    //                     <View style={{ flexDirection: 'row', alignContent: 'flex-end', flex: 2, alignItems: 'center' }}>
-    //                         <Text style={{ fontSize: 12, fontWeight: 'bold', color: '#F7621E', fontStyle: 'italic', marginTop: 4 }}> USD</Text>
-    //                         <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#F7621E', textDecorationStyle: 'dashed', justifyContent: 'flex-end' }}> {prod.precio}</Text>
-    //                     </View>
-    //                     <View style={{ flexDirection: 'row', alignContent: 'flex-start', flex: 0.8, padding: 12 }}>
-    //                         <View style={{ marginRight: 1, marginLeft: 2 }}>
-
-    //                         </View>
-    //                         <View style={{ marginLeft: 10 }}>
-
-    //                         </View>
-
-    //                     </View>
-
-
-
-
-    //                 </View>
-
-    //             </TouchableHighlight>
-
-    //         </ScrollView  >
-
-
-    //     );
-    // }
-
-
-
-
-
-
-
-
 
     return <View style={styles.container}>
         <View style={styles.cajaCabecera} >
 
-            <Text style={{ fontSize: theme.fontSize.title }}>PEDIDOS</Text>
+            <Text style={{ fontSize: theme.fontSize.title }}>PEDIDOS2</Text>
         </View>
         <View style={styles.cajaCuerpo} >
 
